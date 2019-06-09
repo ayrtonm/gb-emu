5c27778733d25d54a7548767c5c015ec2df53081
Ayrton <a.munoz3327@gmail.com>
2019-06-08 22:19:43 -0400

    fixed opcodes 0xf8 and 0xe8
    Now passing cpu_instrs test 03. Only cb opcodes seem to be failing

--- opcodes.h
+++ opcodes.h
@@ -260,7 +260,12 @@ case 0xe5:  {PUSH(hl.b.h,hl.b.l);break;}
 case 0xe6:  {AND(m.read_byte(pc.w-1));break;}
 case 0xe7:  {RST(0x20);break;}
 
-case 0xe8:  {uint16 mtemp = sp.w + ((signed char)m.read_byte(pc.w-1));af.b.l = (mtemp & 0x0100 ? F_C : 0)|((hl.w^(m.read_byte(pc.w-1))^(mtemp & 0xFFFF)) & 0x10 ? F_H : 0);sp.w = mtemp & 0xFFFF;break;}//add sp, r8
+case 0xe8:  {
+  uint16 mtemp = sp.w + ((signed char)m.read_byte(pc.w-1));
+  af.b.l = ((mtemp^sp.w^((signed char)m.read_byte(pc.w-1))) & 0x0100 ? F_C : 0)|((mtemp^sp.w^((signed char)m.read_byte(pc.w-1))) & 0x10 ? F_H : 0);
+  sp.w = mtemp;
+  break;
+}//add sp, r8
 case 0xe9:  {JP(hl.w);break;}
 case 0xea:  {LD_RM(m.read_word(pc.w-2),af.b.h);break;}
 case 0xeb:  {break;}//no opcode
@@ -279,7 +284,12 @@ case 0xf5:  {PUSH(af.b.h,af.b.l);break;}
 case 0xf6:  {OR(m.read_byte(pc.w-1));break;}
 case 0xf7:  {RST(0x30);break;}
 
-case 0xf8:  {int mtemp = sp.w + ((signed char)m.read_byte(pc.w-1));af.b.l = (mtemp & 0x0100 ? F_C : 0)|((hl.w^(m.read_byte(pc.w-1))^(mtemp & 0xFFFF)) & 0x10 ? F_H : 0);LD(hl.w,mtemp & 0xFFFF);break;}//add sp, r8
+case 0xf8:  {
+  uint16 mtemp = sp.w + ((signed char)m.read_byte(pc.w-1));
+  af.b.l = ((mtemp^sp.w^((signed char)m.read_byte(pc.w-1))) & 0x0100 ? F_C : 0)|((mtemp^sp.w^((signed char)m.read_byte(pc.w-1))) & 0x10 ? F_H : 0);
+  hl.w = mtemp;
+  break;
+}//add sp, r8
 case 0xf9:  {LD(sp.w,hl.w);break;}
 case 0xfa:  {LD_MR(af.b.h,m.read_word(pc.w-2));break;}
 case 0xfb:  {ei_delay = 1;break;}

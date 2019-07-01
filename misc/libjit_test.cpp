562ef124b4ddcb175a6305c998d4bcba6c042fa1
Ayrton <a.munoz3327@gmail.com>
2019-07-01 00:48:08 -0400

    added simple libjit example in C++

---
+++ misc/libjit_test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <jit/jit-plus.h>
+
+class myblock : public jit_function {
+  public:
+    myblock(jit_context &context) : jit_function(context) {
+      create();
+      set_recompilable();
+    }
+    virtual void build();
+  protected:
+    virtual jit_type_t create_signature();
+};
+
+void myblock::build() {
+  jit_value x = get_param(0);
+  jit_value y = get_param(1);
+  jit_value z = get_param(2);
+  insn_return(x * y + z);
+}
+
+jit_type_t myblock::create_signature() {
+  return signature_helper(jit_type_int,jit_type_int,jit_type_int,
+                          jit_type_int,end_params); 
+}
+int main(int argc, char *argv[]) {
+  jit_context context;
+  //context.build_start();
+  myblock func(context);
+  func.build();
+  func.compile();
+  //context.build_end();
+
+  jit_int arg1, arg2, arg3;
+  void *args[3];
+  jit_int result;
+  arg1 = 3;
+  arg2 = 5;
+  arg3 = 2;
+  args[0] = &arg1;
+  args[1] = &arg2;
+  args[2] = &arg3;
+  func.apply(args, &result);
+  std::cout << (int)result << std::endl;
+}

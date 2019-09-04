8f5a42008b7abd34dad3c5b442db6631f10f249d
Ayrton <a.munoz3327@gmail.com>
2019-09-04 12:35:48 -0500

    started adding jumps in translate()
    I may need to remove some unconditional jump instructions outside the cache
    blocks. Most jumps, aside from the reset vectors, need to evaluate the state of
    the cpu registers or read from memory. Reading from memory within a block
    should be fine, but evaluating the register state within a block may decrease
    performance. I need to think about how to best use libjit before making a
    decision on this. I will most likely do all register evaluations outside blocks
    even if it means that blocks may be smaller due to all the opcodes that can end
    a block.

--- src/recompiler/dynarec.cpp
+++ src/recompiler/dynarec.cpp
@@ -10,11 +10,17 @@ dynarec_cpu::dynarec_cpu() {
 dynarec_cpu::~dynarec_cpu() {
 }
 void dynarec_cpu::emulate(mem &m, keypad &k, lcd &l, sound &s) {
+  //cache_block *b = new cache_block();
+  //b->store_data(0xff);
+  //b->store_data(0xfe);
+  //b->store_data(0x00);
+  //storage->insert_block(b);
   cache *storage = new cache();
   optional<int> current_idx, next_idx;
-  uint16 current_address, next_address;
+  uint16 current_address = pc.w;
+  uint16 next_address;
 
-  cache_block *block = translate(current_address);
+  cache_block *block = translate(current_address,m);
   current_idx = storage->insert_block(block);
 
   for (;;) {
@@ -22,7 +28,7 @@ void dynarec_cpu::emulate(mem &m, keypad &k, lcd &l, sound &s) {
     next_idx = storage->find_block(next_address);
     //if find_block fails translate the next block
     if (!next_idx) {
-      block = translate(current_address);
+      block = translate(current_address,m);
       next_idx = storage->insert_block(block);
     }
     current_idx = next_idx;
--- src/recompiler/translate.cpp
+++ src/recompiler/translate.cpp
@@ -17,5 +17,108 @@ bool is_jump(uint8 opcode) {
   return find(begin(jumps), end(jumps), opcode) != end(jumps);
 }
 
-cache_block *translate(uint16 start_address) {
+cache_block *translate(uint16 address, mem &m) {
+  cache_block *block = new cache_block();
+  uint8 opcode;
+  do {
+    opcode = m.read_byte(address);
+    block->store_data(opcode);
+    //store opcode arguments if any exist
+    for (int i = 1; i < length[opcode]; i++) {
+      address += 1;
+      block->store_data(m.read_byte(address));
+    }
+    //handle unconditional jump instruction
+    if (is_jump(opcode)) {
+      switch(opcode) {
+        //JR
+        case 0x18: {
+          address += (signed char)m.read_byte(address + 1);
+          break;
+        }
+        //JP
+        case 0xc3: {
+          address = m.read_word(address + 1);
+          break;
+        }
+        //RST 0x00
+        case 0xc7: {
+          //push the program counter onto the stack
+          address = 0x00;
+          break;
+        }
+        //RET
+        case 0xc9: {
+          //pop the program counter from the stack
+          address = 
+          break;
+        }
+        //CALL
+        case 0xcd: {
+          //push the program counter onto the stack
+          address = m.read_word(address + 1);
+          break;
+        }
+        //RST 0x08
+        case 0xcf: {
+          //push the program counter onto the stack
+          address = 0x08;
+          break;
+        }
+        //RST 0x10
+        case 0xd7: {
+          //push the program counter onto the stack
+          address = 0x10;
+          break;
+        }
+        //RETI
+        case 0xd9: {
+          //set IME to 1
+          //pop the program counter from the stack
+          address = 
+          break;
+        }
+        //RST 0x18
+        case 0xdf: {
+          //push the program counter onto the stack
+          address = 0x18;
+          break;
+        }
+        //RST 0x20
+        case 0xe7: {
+          //push the program counter onto the stack
+          address = 0x20;
+          break;
+        }
+        //JP
+        case 0xe9: {
+          //get address from hl registers
+          address = 
+        }
+        //RST 0x28
+        case 0xef: {
+          //push the program counter onto the stack
+          address = 0x28;
+          break;
+        }
+        //RST 0x30
+        case 0xf7: {
+          //push the program counter onto the stack
+          address = 0x30;
+          break;
+        }
+        //RST 0x38
+        case 0xff: {
+          //push the program counter onto the stack
+          address = 0x38;
+          break;
+        }
+        default:
+      }
+    }
+    else {
+      address++;
+    }
+  } while (!is_cond(opcode));
+  return block;
 }

16196e9f6a5ecb5b26b650dbe8bcdd9aaef17c19
Ayrton <a.munoz3327@gmail.com>
2019-09-05 15:38:43 -0500

    added insn_call_native() to libjit test program

--- misc/libjit_test.cpp
+++ misc/libjit_test.cpp
@@ -4,6 +4,11 @@
 int reg = 1;
 jit_type_t type_int_ptr = jit_type_create_pointer(jit_type_int, 0);
 
+int increment_reg() {
+  reg++;
+  return reg;
+}
+
 class myblock : public jit_function {
   public:
     myblock(jit_context &context) : jit_function(context) {
@@ -16,12 +21,12 @@ class myblock : public jit_function {
 };
 
 void myblock::build() {
-  //jit_value arg = get_param(0);
   jit_value constant = new_constant(2, jit_type_int);
   jit_value addr = new_constant(&reg, type_int_ptr);
   jit_value r0 = insn_load_relative(addr, 0, jit_type_int);
   insn_store_relative(addr,0,constant * r0);
-  insn_return();
+  jit_value r1 = insn_call_native(NULL, (void *)&increment_reg, signature(), NULL, 0, 0);
+  insn_return(r1);
 }
 
 jit_type_t myblock::create_signature() {
@@ -29,16 +34,16 @@ jit_type_t myblock::create_signature() {
 }
 int main(int argc, char *argv[]) {
   jit_context context;
-  //context.build_start();
   myblock func(context);
+  context.build_start();
   func.build();
   func.compile();
-  //context.build_end();
+  context.build_end();
 
-  typedef void (*FF)(void);
+  typedef int (*FF)(void);
   FF closure = (FF)func.closure();
   for (int i = 0; i < 10; i++) {
-    closure();
+    std::cout << closure() << " ";
     std::cout << reg << std::endl;
   }
 }

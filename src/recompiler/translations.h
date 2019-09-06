28c060c1ddd2b6c8913de99e4ff8d15ed3e3c65c
Ayrton <a.munoz3327@gmail.com>
2019-09-05 21:44:12 -0500

    added push bc to dynarec version
    I should go through the three opcodes and remove any unused jit_value. I believe
    libjit does this by default but removing as much code as possible will make
    filling in the rest of the opcodes much faster

--- src/recompiler/translations.h
+++ src/recompiler/translations.h
@@ -10,8 +10,22 @@ case 0x01: {
   block.insn_store_relative(bc_addr, 0, word);
   break;
 }
+case 0xc1: {
+  //pop the bc register from the stack
+  jit_value bc_addr = block.new_constant(&bc, type_uint16_ptr);
+  jit_value sp_addr = block.new_constant(&sp, type_uint16_ptr);
+  jit_value bc_reg = block.insn_load_relative(bc_addr, 0, jit_type_ushort);
+  jit_value sp_reg = block.insn_load_relative(sp_addr, 0, jit_type_ushort);
+  jit_value increment = block.new_constant(2, jit_type_int);
+  jit_value_t args[] = {m_addr.raw(), sp_reg.raw()};
+  jit_value temp1 = block.insn_call_native(NULL, (void *)&mem::read_word, read_word_signature, args, 2, 0);
+  jit_value temp2 = sp_reg + increment;
+  block.insn_store_relative(bc_addr, 0, temp1);
+  block.insn_store_relative(sp_addr, 0, temp2);
+  break;
+}
 case 0xc5: {
-  //push bc register onto the stack
+  //push the bc register onto the stack
   jit_value bc_addr = block.new_constant(&bc, type_uint16_ptr);
   jit_value sp_addr = block.new_constant(&sp, type_uint16_ptr);
   jit_value bc_reg = block.insn_load_relative(bc_addr, 0, jit_type_ushort);

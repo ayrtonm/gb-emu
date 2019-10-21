09d25221e6e6534a369606694a7902a0c196f520
Ayrton <a.munoz3327@gmail.com>
2019-10-21 01:06:43 -0500

    listed opcodes needed to run pong demo with the dynarec in translations.h
    also synced with assembler repo

--- src/recompiler/translations.h
+++ src/recompiler/translations.h
@@ -9,6 +9,72 @@ case 0x01: {
   block.insn_store_relative(bc_addr, 0, word);
   break;
 }
+case 0x11: {
+  break;
+}
+case 0x21: {
+  break;
+}
+case 0x22: {
+  break;
+}
+case 0x25: {
+  break;
+}
+case 0x2a: {
+  break;
+}
+case 0x2d: {
+  break;
+}
+case 0x2e: {
+  break;
+}
+case 0x2f: {
+  break;
+}
+case 0x3e: {
+  break;
+}
+case 0x47: {
+  break;
+}
+case 0x4f: {
+  break;
+}
+case 0x5: {
+  break;
+}
+case 0x56: {
+  break;
+}
+case 0x5f: {
+  break;
+}
+case 0x6: {
+  break;
+}
+case 0x72: {
+  break;
+}
+case 0x78: {
+  break;
+}
+case 0x7b: {
+  break;
+}
+case 0x80: {
+  break;
+}
+case 0x90: {
+  break;
+}
+case 0x97: {
+  break;
+}
+case 0xb8: {
+  break;
+}
 case 0xc1: {
   //pop the bc register from the stack
   //pop $bc
@@ -36,5 +102,35 @@ case 0xc5: {
   block.insn_store_relative(sp_addr, 0, sp_reg);
   jit_value_t args[] = {m_addr.raw(), sp_reg.raw(), bc_reg.raw()};
   block.insn_call_native(NULL, (void *)&mem::write_word, write_word_signature, args, 3, 0);
+  break;
+}
+case 0xc6: {
+  break;
+}
+case 0xd1: {
+  break;
+}
+case 0xd5: {
+  break;
+}
+case 0xd6: {
+  break;
+}
+case 0xe1: {
+  break;
+}
+case 0xe5: {
+  break;
+}
+case 0xe6: {
+  break;
+}
+case 0xea: {
+  break;
+}
+case 0xfa: {
+  break;
+}
+case 0xfe: {
   break;
 }
